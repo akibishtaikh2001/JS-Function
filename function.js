4616caf3f9e1bd15bb99f378132bc985ed519ed1
akibishtaikh2001 <akibishtiakh2220@gmail.com>
2025-08-04 17:53:45 +0600

    Function introduction and call

---
+++ function.js
@@ -0,0 +1,26 @@
+const money = 45;
+
+// function declare
+function fanoffkor() {
+    console.log('bosa theke uthe dara');
+    console.log('walk towards the switch');
+    console.log('click the switch to off of thr=e Fan')
+}
+
+// Call the function
+  fanoffkor();
+
+  
+//  function name(){
+
+// }
+
+function brushTeeth(){
+    console.log('Pick up the brash');
+    console.log('add paste');
+    console.log('2 min goso and goso and fana felo'); 
+    
+}
+
+// call the function
+brushTeeth();
