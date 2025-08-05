4b21eb4742ac49f6139b1bbb456518bc8a1b8cf9
akibishtaikh2001 <akibishtiakh2220@gmail.com>
2025-08-05 12:16:52 +0600

    Conditional Return

---
+++ conditional_return.js
@@ -0,0 +1,24 @@
+function isEven(number){
+    if(number % 2 === 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+
+}
+
+// console.log(isEven(5));
+// console.log(isEven(110));
+
+function issOdd(number){
+    if(number % 2 === 1){
+        return true;
+    }
+    return false;
+}
+
+console.log(issOdd(10));
+
+console.log(issOdd(7));
+
