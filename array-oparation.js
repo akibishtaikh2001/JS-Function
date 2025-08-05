906c60b449805d88307959011060b243c5b8cec1
akibishtaikh2001 <akibishtiakh2220@gmail.com>
2025-08-05 21:46:05 +0600

    Array Oparation

---
+++ array-oparation.js
@@ -0,0 +1,22 @@
+/**
+ * Objective: write a function to give me the sum of all numbers in an array
+ * step - 1: declare a function
+ * step - 2: Call check wheather the function is called properly
+ * step - 3: set a parameter(s)
+ * step - 4: pas the parameter(s), check whether parameter is passed in a proper format
+ * step - 5: do the function tasks (step by step)
+ */
+
+function sumOfNumbers(numbers) {
+    let sum = 0;
+    for (const number of numbers) {
+        console.log(number);
+        sum = sum + number;
+
+    }
+    return sum;
+
+}
+const nums = [4, 6, 1, 6];
+const sum = sumOfNumbers(nums);
+console.log('sum of number is', sum);
